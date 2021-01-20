67e521b5fbdcf13518bd3b3210bdfab0b7115efe
Diana Vlasenkova <dianadomino24work@gmail.com>
2021-01-20 16:47:19 +0300

    fix search form empty input with only spaces

--- news-explorer-frontend/src/components/SearchForm/SearchForm.js
+++ news-explorer-frontend/src/components/SearchForm/SearchForm.js
@@ -1,10 +1,17 @@
 import './SearchForm.css';
 import { useState } from 'react';
 import Button from '../Button/Button';
+import useForm from '../../hooks/useForm';
 
 function SearchForm({ setSearchState }) {
   const warningMessage = 'Please, fill in a keyword';
   const [isInputEmpty, setIsInputEmpty] = useState(false);
+
+  const {
+    values,
+    handleChange,
+    resetForm,
+  } = useForm();
 
   const handleFocus = () => {
     setIsInputEmpty(false);
@@ -12,9 +19,10 @@ function SearchForm({ setSearchState }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    if (!e.target.searchForm.value.trim()) {
+    if (!e.target.searchForm.value.replace(/\s/g, '').trim().length) {
+      e.target.searchForm.value = '';
       setIsInputEmpty(true);
+      resetForm();
       return;
     }
     // !!!!!!!! temporary
@@ -26,10 +34,12 @@ function SearchForm({ setSearchState }) {
             <input
                 type="text"
                 name="searchForm"
+                value={values.search}
                 className={`search-form__input ${isInputEmpty && 'search-form__input_warning'}`}
                 placeholder={isInputEmpty ? warningMessage : 'Fill in the search keywords'}
                 required
                 onFocus={handleFocus}
+                onChange={handleChange}
             />
             <Button buttonClasses="button_type_text button_type_submit search-form__button" type="submit">
                 Search
